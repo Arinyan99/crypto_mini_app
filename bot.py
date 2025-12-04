9031b5a34d950308b44c416488ccdb41dcb078ed
Arinyan99 <heyingtop7@gmail.com>
2025-12-05 01:01:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
+import telebot
+
+bot = telebot.TeleBot("PASTE_YOUR_TOKEN_HERE")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Бот запущен на Render!")
+
+bot.polling()
 
