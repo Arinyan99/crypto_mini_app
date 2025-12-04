5a923c113346ce8d5197a12074ed321041240f3f
Arinyan99 <heyingtop7@gmail.com>
2025-12-05 01:40:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot("8513936915:AAG0Jw8hdbX6TwTqduCUNc9PVUZOtTjpbBU")
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Бот запущен на Render!")
+    bot.send_message(message.chat.id, "Приветствую в крипто-боте!")
 
 bot.polling()
 
